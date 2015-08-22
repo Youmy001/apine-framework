74a4c20a3f4c576048d87927bdee1d8274117ffa
Youmy001 <tteasdaleroads@gmail.com>
2015-08-22 16:26:39 -0400

    Added Translation module

---
+++ lib/translation/translation.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Langauage Translation
+ * This script contains a representation the translation of a language for Translator core module
+ *
+ * @license MIT
+ * @copyright 2015 Tommy Teasdale
+ */
+
+/**
+ * Language Translation
+ * Representation of a translations for a language
+ */
+class Translation {
+
+	/**
+	 * Representation of a language for translation
+	 * 
+	 * @var TranslationLanguage
+	 */
+	private $language;
+	
+	/**
+	 * Translation strings extracted from the translation file
+	 * 
+	 * @var array
+	 */
+	private $entries;
+	
+	/**
+	 * Construct the Translation handler
+	 * Extract string from the translation file
+	 * 
+	 * @param TranslationLanguage $a_language
+	 * @throws Exception If the file is inexistant or invalid
+	 */
+	public function __construct(TranslationLanguage $a_language) {
+		
+		$this->language = $a_language;
+		
+		if (file_exists($this->language->file_path)) {
+			$file=new File($this->language->file_path);
+			
+			if($file->get_extention()=="json"){
+				$content=fread($file->handle(),$file->get_size());
+				$content=json_decode($content);
+				$array=array();
+				
+				foreach ($content as $part=>$sub_content) {
+					if (is_string($sub_content)){
+						$array[$part] = $sub_content;
+					} else {
+						foreach ($sub_content as $key=>$value) {
+							$array[$part][$key] = $value;
+						}
+					}
+				}
+				
+				$this->entries=$array;
+			}else{
+				throw new Exception("Invalid File");
+			}
+		}else{
+			throw new Exception("Inexistant File");
+		}
+		
+	}
+	
+	/**
+	 * Fetch a translation string
+	 * 
+	 * @param string $a_prefix
+	 * @param string $a_key
+	 * @return string
+	 */
+	public function get($a_prefix, $a_key=null) {
+		
+		$prefix = strtolower($a_prefix);
+		
+		if ($a_key!=null) {
+			$key = strtolower($a_key);
+			return isset($this->entries[$prefix][$key]) ? $this->entries[$prefix][$key] : null;
+		} else {
+			return isset($this->entries[$prefix]) ? $this->entries[$prefix] : null;
+		}
+		
+	}
+	
+	/**
+	 * Fetch a translation string with a format
+	 * 
+	 * @param string $a_key
+	 * @param string $a_index
+	 * @param string $a_pattern
+	 * @return string
+	 */
+	public function parse($a_key, $a_index=null, $a_pattern=null) {
+		
+		if (!is_array($a_pattern)) {
+			$a_pattern = array($a_pattern);
+		}
+		return call_user_func_array('sprintf', array_merge(array(
+						$this->get($a_key, $a_index)
+		), $a_pattern));
+		
+	}
+	
+	/**
+	 * Return the Translation Language
+	 * 
+	 * @return TranslationLanguage
+	 */
+	public function get_language() {
+		
+		return $this->language;
+		
+	}
+	
+}
---
+++ lib/translation/translator.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * Translator Tool
+ * This script contains a facade for the Translator core module
+ *
+ * @license MIT
+ * @copyright 2015 Tommy Teasdale
+ */
+
+/**
+ * Language Translator Tool
+ * Manage multi-language configuration
+ */
+class Translator {
+	
+	/**
+	 * Availables languages for translations
+	 * 
+	 * @var TranslationLanguage[]
+	 */
+	private $languages;
+	
+	/**
+	 * Translations for available languages
+	 * 
+	 * @var Translation[]
+	 */
+	private $translations;
+	
+	/**
+	 * Instance of the Translator
+	 * Singleton Implementation
+	 *
+	 * @var Translator
+	 */
+	private static $_instance;
+	
+	/**
+	 * Construct the Translator handler
+	 * Finding available language from language files
+	 */
+	private function __construct(){
+		
+		$this->languages = array();
+		$this->translations = array();
+		
+		//$array= [];
+		$array=array();
+		
+		// Find and instantiate every languages
+		foreach (scandir('resources/languages/') as $file) {
+			if ($file != "." && $file != "..") {
+				$file_name=explode(".", $file);
+				$file_name=$file_name[0];
+				$this->languages[$file_name] = new TranslationLanguage($file_name,'resources/languages/'.$file);
+			}
+		}
+		
+	}
+	
+	/**
+	 * Singleton design pattern implementation
+	 *
+	 * @static
+	 * @return Translator
+	 */
+	public static function get_instance() {
+		
+		if (!isset(self::$_instance)) {
+			self::$_instance = new static();
+		}
+		
+		return self::$_instance;
+		
+	}
+	
+	/**
+	 * Fetch a list of every translation language available 
+	 * 
+	 * @return TranslationLanguage[]
+	 */
+	public static function get_all_languages() {
+		
+		// Return a liste of every language
+		return self::get_instance()->languages;
+		
+	}
+	
+	/**
+	 * Fetch an available translation language
+	 * 
+	 * @param string $a_language_code
+	 * @return TranslationLanguage
+	 */
+	public static function get_language($a_language_code) {
+		
+		// Return a language whose code maches the parameter
+		return isset(self::get_instance()->languages[$a_language_code]) ? self::get_instance()->languages[$a_language_code] : null;
+		
+	}
+	
+	/**
+	 * Verify if a language is available
+	 * 
+	 * @param string $a_language_code
+	 * @return boolean
+	 */
+	public static function is_exist_language($a_language_code) {
+		
+		// Verify if a language matches the parameter
+		//return self::get_instance()->languages->key_exists($a_language_code);
+		return isset(self::get_instance()->languages[$a_language_code]);
+		
+	}
+	
+	/**
+	 * Fetch a translation
+	 * 
+	 * @param string $a_language_code
+	 * @return Translation
+	 */
+	public static function get_translation($a_language_code) {
+		
+		// Load and return translation string of a language whose code matches the parameter
+		if (isset(self::get_instance()->languages[$a_language_code])) {
+			if(!isset(self::get_instance()->translations[$a_language_code])) {
+				self::get_instance()->translations[$a_language_code] = new Translation(self::get_instance()->languages[$a_language_code]);
+			}
+			
+			return self::get_instance()->translations[$a_language_code];
+		} else {
+			return null;
+		}
+		
+	}
+	
+	/**
+	 * Fetch a translation string
+	 * 
+	 * @param string $a_language_code
+	 * @param string $a_key
+	 * @param string $a_index
+	 * @return string
+	 */
+	public static function translate($a_language_code, $a_key, $a_index=null) {
+		
+		// Return a translation string of a matching language whose key and index matches parameters
+		$translation=self::get_instance()->get_translation($a_language_code);
+		
+		if ($translation!=null) {
+			return $translation->get($a_key, $a_index);
+		} else {
+			return null;
+		}
+		
+	}
+	
+	/**
+	 * Fetch a translation string with a format
+	 * 
+	 * @param string $a_language_code
+	 * @param string $a_key
+	 * @param string $a_index
+	 * @param string $a_pattern
+	 * @return string
+	 */
+	public static function translate_parse($a_language_code, $a_key, $a_index=null, $a_pattern=null) {
+		
+		// Fetch a translation string of a matching language whose key and index matches parameters
+		// and return a formatted version from a pattern is provided
+		$translation=self::get_instance()->get_translation($a_language_code);
+		
+		if ($translation!=null) {
+			return $translation->parse($a_key, $a_index, $a_pattern);
+		} else {
+			return null;
+		}
+		
+	}
+	
+}
