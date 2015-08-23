42413c492035a7a6666b90d251efd71daed250cd
Youmy001 <tteasdaleroads@gmail.com>
2015-08-23 10:50:23 -0400

    Remove references to nonexistent language ni views

--- views/session/login.php
+++ views/session/login.php
@@ -4,12 +4,12 @@
 
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				<h3 class="panel-title"><?= Translator::translate('en-US', 'login', 'title') ?></h3>
+				<h3 class="panel-title">Login</h3>
 			</div>
 			<?php if ($this->_params->get_item('error_code')) :?>
 			<div class="alert alert-block alert-warning" style="margin:0;border-radius:0;">
 				<button type="button" class="close" data-dismiss="alert">&times;</button>
-				<h4><strong><?= Translator::translate('en-US', 'form', 'warning') ?></strong></h4>
+				<h4><strong>Warning !</strong></h4>
 				<span><?php echo $this->_params->get_item('error_message'); ?></span>
 			</div>
 			<?php endif;?>
@@ -17,22 +17,22 @@
 				<form id="content" action="<?php echo URL_Helper::path("login",false);?>"
 					method="post">
 					<div class="form-group">
-						<label class="control-label"><?= Translator::translate('en-US', 'login', 'username_email') ?></label>
+						<label class="control-label">Username or Email</label>
 						<input class="form-control" type="text" name="user"
-								placeholder="<?= Translator::translate('en-US', 'login', 'username') ?>" required />
+								placeholder="Username" required />
 					</div>
 					<div class="form-group">
-						<label class="control-label"><?= Translator::translate('en-US', 'login', 'password') ?></label>
+						<label class="control-label">Password</label>
 						<input class="form-control" type="password" name="pwd"
-								placeholder="<?= Translator::translate('en-US', 'login', 'password') ?>" required />
+								placeholder="Password" required />
 					</div>
 					<div class="checkout">
 						<label>
-							<input type="checkbox" name="perm" value="on"> <?= Translator::translate('en-US', 'login', 'remember') ?>
+							<input type="checkbox" name="perm" value="on"> Remember Me
 						</label>
-						<button type="submit" class="btn btn-primary pull-right"><?= Translator::translate('en-US', 'login', 'submit') ?></button>
+						<button type="submit" class="btn btn-primary pull-right">Login</button>
 					</div>
-					<a href="<?php echo URL_Helper::path("login/restore");?>"><?= Translator::translate('en-US', 'login', 'forgot') ?></a>
+					<a href="<?php echo URL_Helper::path("login/restore");?>">Forgot your password?</a>
 				</form>
 			</div>
 		</div>
--- views/session/register.php
+++ views/session/register.php
@@ -4,12 +4,12 @@
 
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				<h3 class="panel-title"><?= Translator::translate('en-US', 'register', 'title') ?></h3>
+				<h3 class="panel-title">Sign Up</h3>
 			</div>
 			<?php if ($this->_params->get_item('error_code')) :?>
 			<div class="alert alert-block alert-warning" style="margin:0;border-radius:0;">
 				<button type="button" class="close" data-dismiss="alert">&times;</button>
-				<h4><strong><?= Translator::translate('en-US', 'form', 'warning') ?></strong></h4>
+				<h4><strong>Warning !</strong></h4>
 				<span><?php echo $this->_params->get_item('error_message'); ?></span>
 			</div>
 			<?php endif;?>
@@ -17,22 +17,22 @@
 				<form id="content" action="<?php echo URL_Helper::path("register",false);?>"
 					method="post">
 					<div class="form-group">
-						<label class="control-label"><?= Translator::translate('en-US', 'register', 'username') ?></label>
-						<input class="form-control" type="text" name="user" placeholder="<?= Translator::translate('en-US', 'register', 'username') ?>" required />
+						<label class="control-label">Username</label>
+						<input class="form-control" type="text" name="user" placeholder="Username" required />
 					</div>
 					<div class="form-group">
-						<label class="control-label"><?= Translator::translate('en-US', 'register', 'password') ?></label>
-						<input class="form-control" type="password" name="pwd" placeholder="<?= Translator::translate('en-US', 'register', 'password') ?>" required />
+						<label class="control-label">Password</label>
+						<input class="form-control" type="password" name="pwd" placeholder="Password" required />
 					</div>
 					<div class="form-group">
-						<label class="control-label"><?= Translator::translate('en-US', 'register', 'password_confirm') ?></label>
-						<input class="form-control" type="password" name="pwd_confirm" placeholder="<?= Translator::translate('en-US', 'register', 'password') ?>" required />
+						<label class="control-label">Confirm Password</label>
+						<input class="form-control" type="password" name="pwd_confirm" placeholder="Password" required />
 					</div>
 					<div class="form-group">
-						<label class="control-label"><?= Translator::translate('en-US', 'register', 'email') ?></label>
+						<label class="control-label">Email Address</label>
 						<input class="form-control" type="email" name="email" placeholder="example@example.com"/>
 					</div>
-					<button type="submit" class="btn btn-primary pull-right"><?= Translator::translate('en-US', 'register', 'submit') ?></button>
+					<button type="submit" class="btn btn-primary pull-right">Sign Up</button>
 				</form>
 			</div>
 		</div>
