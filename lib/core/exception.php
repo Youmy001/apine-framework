ffe8f2b8420973180ebc7d79ec311012150547ef
Youmy001 <tteasdaleroads@gmail.com>
2015-09-29 01:12:35 -0400

    Custom Exception Handler

---
+++ lib/core/exception.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Custom Exception Handler
+ * This script contains a custom exception handler
+ *
+ * @license MIT
+ * @copyright 2015 Tommy Teasdale
+ */
+
+/**
+ * Custom implementation of the exception handler matching
+ * more closely a RESTful approach
+ * 
+ * @author Tommy Teasdale
+ */
+class ApineException extends Exception {
+
+	protected $code = 500;
+
+	public function __construct($message = null, $code = 500, Exception $previous = null){
+
+		parent::__construct($message, $code, $previous);
+
+	}
+
+}
