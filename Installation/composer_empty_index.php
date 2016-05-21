0c32466b16486e3bb7a3db3b4ea23fd3dbed8306
Youmy001 <tteasdaleroads@gmail.com>
2016-05-21 15:55:35 -0400

    Add directory separator support in installation templates

--- Installation/composer_empty_index.php
+++ Installation/composer_empty_index.php
@@ -10,7 +10,7 @@
 
 ini_set('display_errors', -1);
 
-require_once 'vendor/autoload.php';
+require_once 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 $loader = new Apine\Autoloader();
 $loader->register();
