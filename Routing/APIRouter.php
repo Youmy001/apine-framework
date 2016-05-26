5eadb9476f90e6c15c8532412bde2176e7bbdefe
Youmy001 <tteasdaleroads@gmail.com>
2016-05-25 22:57:03 -0400

    Fix typo

--- Routing/APIRouter.php
+++ Routing/APIRouter.php
@@ -71,7 +71,7 @@ final class APIRouter implements RouterInterface {
 			$maj_controller = ucfirst($controller) . 'Controller';
 			$return = false;
 			
-			if (class_exists('Apine\\Controllers\\User\\' . $maj_controller) && in_array('Apine\\MVC\\APIActionsInterface', class_implements('Apine\\Controllers\\' . $maj_controller))) {
+			if (class_exists('Apine\\Controllers\\User\\' . $maj_controller) && in_array('Apine\\MVC\\APIActionsInterface', class_implements('Apine\\Controllers\\User\\' . $maj_controller))) {
 				$return = 'Apine\\Controllers\\User\\' . $maj_controller;
 			} else if (class_exists('Apine\\Controllers\\System\\' . $maj_controller) && in_array('Apine\\MVC\\APIActionsInterface', class_implements('Apine\\Controllers\\System\\' . $maj_controller))) {
 				$return = 'Apine\\Controllers\\System\\' . $maj_controller;
